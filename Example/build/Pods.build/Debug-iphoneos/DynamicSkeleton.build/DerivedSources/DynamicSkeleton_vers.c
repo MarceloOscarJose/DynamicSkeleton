d057bf108d6ab9d4c480f5735bed8883cc9807e2
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2017-07-29 19:05:50 -0300

    Remove some files

---
+++ Example/build/Pods.build/Debug-iphoneos/DynamicSkeleton.build/DerivedSources/DynamicSkeleton_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char DynamicSkeletonVersionString[];
+ extern const double DynamicSkeletonVersionNumber;
+
+ const unsigned char DynamicSkeletonVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DynamicSkeleton  PROJECT:Pods-1" "\n";
+ const double DynamicSkeletonVersionNumber __attribute__ ((used)) = (double)1.;
